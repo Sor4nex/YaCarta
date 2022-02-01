3e7d6e7deea46be2930b0a84e0281058dc21e5a5
Sor4nex <AXSoran47@yandex.ru>
2022-02-01 18:52:31 +0300

    1

--- main.py
+++ main.py
@@ -1,54 +1,9 @@
-import pygame
 import requests
 import sys
 import os
 import math
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
-
-SCREEN_SIZE = [600, 450]
-
-
-class Image(QWidget):
-    def __init__(self):
-        super().__init__()
-
-    def get_coor(self, a, b):
-        self.cor_1 = a
-        self.cor_2 = b
-        self.getImage()
-        self.initUI()
-        self.show()
-
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=" + str(self.cor_1) + ',' + str(self.cor_2) + "&spn=20,20&l=sat"
-        response = requests.get(map_request)
-
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
-
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
 
 
 class Example(QWidget):
@@ -109,6 +64,5 @@ class Example(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
-    im = Image()
     ex.show()
     sys.exit(app.exec())
