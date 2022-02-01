2db8730e93e1120634919d1a61a20b58595c181b
Sor4nex <AXSoran47@yandex.ru>
2022-02-01 18:26:44 +0300

    Main window added and styled

--- main.py
+++ main.py
@@ -14,22 +14,30 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 700, 300)
+        self.setGeometry(300, 300, 700, 700)
         self.setWindowTitle('Карта')
 
         self.out1 = QLineEdit(self)
-        self.out1.resize(250, 50)
-        self.out1.move(25, 25)
-        self.out1.setText('Координата 1')
+        self.out1.resize(250, 30)
+        self.out1.move(50, 25)
+        self.out1.setPlaceholderText('Координата 1')
 
         self.out1 = QLineEdit(self)
-        self.out1.resize(250, 50)
+        self.out1.resize(250, 30)
         self.out1.move(325, 25)
-        self.out1.setText('Координата 2')
+        self.out1.setPlaceholderText('Координата 2')
+
+        self.txt1 = QLabel(self)
+        self.txt1.setText('координата 1:')
+        self.txt1.move(50, 5)
+
+        self.txt2 = QLabel(self)
+        self.txt2.setText('координата 2:')
+        self.txt2.move(325, 5)
 
         self.btn = QPushButton('-->', self)
-        self.btn.resize(500, 100)
-        self.btn.move(100, 150)
+        self.btn.resize(550, 50)
+        self.btn.move(50, 600)
         self.btn.clicked.connect(self.clic)
 
     def clic(self):
