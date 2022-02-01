8df12788a7478508fe0815341f4695ee08e8df45
Сафронов Иван <93474223+IvanSafronov2006@users.noreply.github.com>
2022-02-01 18:30:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,50 @@ import math
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
 
+SCREEN_SIZE = [600, 450]
+
+
+class Image(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def get_coor(self, a, b):
+        self.cor_1 = a
+        self.cor_2 = b
+        self.getImage()
+        self.initUI()
+        self.show()
+
+    def getImage(self):
+        map_request = "http://static-maps.yandex.ru/1.x/?ll=" + str(self.cor_1) + ',' + str(self.cor_2) + "&spn=20,20&l=sat"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
 
 class Example(QWidget):
     def __init__(self):
@@ -15,17 +59,17 @@ class Example(QWidget):
 
     def initUI(self):
         self.setGeometry(300, 300, 700, 700)
-        self.setWindowTitle('Карта')
+        self.setWindowTitle('Панель управления')
 
         self.out1 = QLineEdit(self)
         self.out1.resize(250, 30)
         self.out1.move(50, 25)
         self.out1.setPlaceholderText('Координата 1')
 
-        self.out1 = QLineEdit(self)
-        self.out1.resize(250, 30)
-        self.out1.move(325, 25)
-        self.out1.setPlaceholderText('Координата 2')
+        self.out2 = QLineEdit(self)
+        self.out2.resize(250, 30)
+        self.out2.move(325, 25)
+        self.out2.setPlaceholderText('Координата 2')
 
         self.txt1 = QLabel(self)
         self.txt1.setText('координата 1:')
@@ -41,11 +85,17 @@ class Example(QWidget):
         self.btn.clicked.connect(self.clic)
 
     def clic(self):
-        cor_x = 0
+        try:
+            cor_x = float(self.out1.text())
+            cor_y = float(self.out2.text())
+            im.get_coor(cor_x, cor_y)
+        except:
+            pass
         
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
+    im = Image()
     ex.show()
     sys.exit(app.exec())
