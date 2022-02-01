e4aba03bb22c616bca3be1b15f8aaa07a5bda1ff
MorroBludvist <93429764+MorroBludvist@users.noreply.github.com>
2022-02-01 21:37:34 +0300

    Update main.py
    bagfixes

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 import math
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
-from PyQt5.Qt import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.Qt import *
 SCREEN_SIZE = [600, 450]
 
 
@@ -15,9 +16,8 @@ class Example(QWidget):
 
     def initUI(self):
         self.x = 100  # Координаты центра карты на старте
-        self.y = 100  # Координаты центра карты на стартe
-        self.zoom = 15  # Мастштаб
-        self.spn = 20
+        self.y = 100  # Координаты центра карты на старте
+        self.zoom = 10  # Мастштаб
         self.type_map = 'map'  # Тип карты
         self.setGeometry(300, 300, 700, 700)
         self.setWindowTitle('Панель управления')
@@ -49,11 +49,20 @@ class Example(QWidget):
         self.btn.move(50, 600)
         self.btn.clicked.connect(self.clic)
 
+    def to_ll(self):
+        return "{0},{1}".format(self.x, self.y)
+
     def clic(self):
         try:
             cor_x = float(self.out1.text())
             cor_y = float(self.out2.text())
             self.getImage(cor_x, cor_y)
+        except:
+            pass
+
+    def clic2(self):
+        try:
+            self.getImage(self.x, self.y)
         except:
             pass
 
@@ -71,25 +80,27 @@ class Example(QWidget):
         self.pix = self.pix.scaled(500, 500)
         self.res_map.setPixmap(self.pix)
 
-        def keyPressEvent(self, event):
+    def keyPressEvent(self, event):
+        print(event.key())
         if event.key() == Qt.Key_PageUp and self.zoom < 16:
             self.zoom += 1
-            self.clic()
-        elif event.key() == Qt.Key_PageDown and self.zoom > 1:
+            self.clic2()
+        elif event.key() == Qt.Key_PageDown and self.zoom > 2:
             self.zoom -= 1
-            self.clic()
+            self.clic2()
         elif event.key() == Qt.Key_A:
-            self.x -= 0.005 * 2 ** (16 - self.zoom)
-            self.clic()
+            self.x -= 0.005 * 2 ** (15 - self.zoom)
+            self.clic2()
         elif event.key() == Qt.Key_D:
-            self.x += 0.005 * 2 ** (16 - self.zoom)
-            self.clic()
+            self.x += 0.005 * 2 ** (15 - self.zoom)
+            self.clic2()
         elif event.key() == Qt.Key_W and self.y < 90:
-            self.y += 0.005 * 2 ** (16 - self.zoom)
-            self.clic()
+            self.y += 0.005 * 2 ** (15 - self.zoom)
+            self.clic2()
         elif event.key() == Qt.Key_S and self.y > -90:
-            self.y -= 0.005 * 2 ** (16 - self.zoom)
-            self.clic()
+            self.y -= 0.005 * 2 ** (15 - self.zoom)
+            self.clic2()
+        print(self.x, self.y)
 
         if self.x > 180:
             self.x -= 360
